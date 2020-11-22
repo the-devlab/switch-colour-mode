753bcb5fbbacc47d9e2b5f225256e0f300b6ea76
the-devlab <74771028+the-devlab@users.noreply.github.com>
2020-11-22 14:06:23 +0000

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -24,12 +24,12 @@ for (var i = 0; modeLinks.length > i; i++) {
 
 function setTheme(mode) {
   if (mode == 'dark') {
-    themeStyle.href = 'dark.css';
+    themeStyle.href = 'styles/dark.css';
     modeText.innerHTML = 'Dark Mode';
     darkMode.style = 'display: none;';
     lightMode.style = 'display: inline-block;';
   } else {
-    themeStyle.href = 'light.css';
+    themeStyle.href = 'styles/light.css';
     modeText.innerHTML = 'Light Mode';
     lightMode.style = 'display: none;';
     darkMode.style = 'display: inline-block;';
